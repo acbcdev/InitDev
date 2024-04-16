f4bb54f2722e414174c0a6f7dd37aa8525fcb552
acbc <acbc.dev@gmail.com>
2024-04-16 15:19:55 -0500

    chore: Add scripts for installing dependencies, initializing aliases, and configuring git

--- main.sh
+++ main.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+echo "Instalando dependencias"
 ./components/installDevPendecias.sh
+echo "Instalando programas by Curl"
 ./components/installByCurl.sh
+echo "Instalando Node"
 ./components/installNode.sh
+echo "Creando directorios de desarrollo"
 mkdir Dev
 mkdir Dev/Proyectos
 mkdir Dev/Platzi
 mkdir Dev/Cmt
+echo "Init Alias "
 ./components/initAlias.sh
+echo "Configurando Git"
 ./components/gitconfi.sh

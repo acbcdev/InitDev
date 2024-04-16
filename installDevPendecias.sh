e3b26b6059fc26cf5acb72b875d86a11bf2ca513
acbc <acbc.dev@gmail.com>
2024-04-16 14:32:09 -0500

    chore: Add script to install development dependencies
    This commit adds a new script, `installDevPendecias.sh`, which installs the necessary development dependencies. The user is prompted to choose their package manager (dnf, apt, or pacman), and the script installs the dependencies accordingly. The dependencies include git, curl, zsh, ranger, lolcat, and exa.
    
    The script also includes error handling for invalid package managers.
    
    Note: This commit message follows the conventional commit format, starting with a type (chore) and providing a concise and descriptive summary of the changes made.

---
+++ installDevPendecias.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Instalando dependencias
+# selecionar admin de devpendencias dnf apt pacman
+# dependencias git Curl zsh ranger lolcat exa
+dependencias="git curl zsh ranger lolcat exa"
+read -rp "Choose your package manager (dnf pacman apt): " package_manager
+
+while [ "$package_manager" != "dnf" ] && [ "$package_manager" != "apt" ] && [ "$package_manager" != "pacman" ]; do
+  echo "Invalid package manager"
+  read -rp "Choose your package manager (dnf pacman apt): " package_manager
+done
+if [ "$package_manager" == "dnf" ]; then
+  sudo dnf update
+  sudo dnf install $dependencias
+elif [ "$package_manager" == "apt" ]; then
+  sudo apt update
+  sudo apt install $dependencias
+elif [ "$package_manager" == "pacman" ]; then
+  sudo pacman -Syu
+  sudo pacman -S $dependencias
+else
+  echo "Invalid package manager"
+  exit 1
+fi

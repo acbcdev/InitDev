312a30590e5f926cb665eea14fb21234add2cfa3
ACBC <acbc.dev@gmail.com>
2025-01-07 10:36:00 -0500

    fix: remove unnecessary whitespace in installApps.sh

--- components/installApps.sh
+++ components/installApps.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
 
 dnf check-update
+
 sudo dnf install code code-insiders
 
 flatpak install flathub com.brave.Browser

3b121ddbb258fc04a71f2418d36dc236646068f2
acbc <acbc.dev@gmail.com>
2025-01-08 12:17:44 -0500

    fix: add zoxide to dependencies and update .zshrc initialization in installDevPendecias.sh

--- components/installDevPendecias.sh
+++ components/installDevPendecias.sh
@@ -4,7 +4,7 @@
 # selecionar admin de devpendencias dnf apt pacman
 # dependencias git Curl zsh ranger lolcat exa
 
-dependencias="curl ranger z lolcat exa fastfetch "
+dependencias="curl ranger zoxide lolcat exa fastfetch "
 read -rp "Choose your package manager (dnf pacman apt): " package_manager
 
 while [ "$package_manager" != "dnf" ] && [ "$package_manager" != "apt" ] && [ "$package_manager" != "pacman" ]; do
@@ -17,18 +17,20 @@ if [ "$package_manager" == "dnf" ]; then
   sudo dnf install "$dependencias"
   sudo dnf copr enable atim/lazygit -y
   sudo dnf install lazygit
+  echo eval "$(zoxide init zsh)" >> ~/.zshrc
   echo "Instalando gh"
   ./installGh.sh dnf
 
 elif [ "$package_manager" == "apt" ]; then
-
   sudo apt update
   sudo apt install "$dependencias"
+  echo eval "$(zoxide init zsh)" >> ~/.zshrc
   echo "Instalando gh"
   ./installGh.sh apt
 elif [ "$package_manager" == "pacman" ]; then
   sudo pacman -Syu
   sudo pacman -S "$dependencias"
+  echo eval "$(zoxide init zsh)" >> ~/.zshrc
   echo "Instalando gh"
   ./installGh.sh pacman
 else

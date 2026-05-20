f9137ddf092972f8bc90651c0b0daeff9616d4c0
acbc <acbc.dev@gmail.com>
2026-05-19 22:13:03 -0500

    chore(cleanup): remove legacy root scripts
    Delete outdated standalone scripts replaced by the modular lib/ architecture.

--- installOhmyzsh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -d "$HOME/.oh-my-zsh" ]; then
-  echo "Oh my zsh ya esta instalado"
-else
-  echo "Instalando oh my zsh"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
